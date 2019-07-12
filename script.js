21e05f8e3835026711469bbc72f92db5f3be9e90
anshi2007 <52678769+anshi2007@users.noreply.github.com>
2019-07-12 13:54:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var size = 10;
 setInterval(function(){
-  document.getElementById("my text").style.fontSize= size + "px";
+  document.getElementById("mytext").style.fontSize= size + "px";
   size = size + 5; 
 }, 1000);
